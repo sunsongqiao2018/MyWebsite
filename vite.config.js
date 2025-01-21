acf9a778d3a1a287897432e65e8adee7de4da46d
Songqiao Sun <38797477+sunsongqiao2018@users.noreply.github.com>
2025-01-20 22:50:52 -0500

    Revert "url routing"
    This reverts commit 0fd417619d196eac20bb6eb10b4b54cab10726ac.

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/home/',
+  base: '/',
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
-  },
 }) 
